b2a2de4fa5b7627532020f83be03183159fb303c
Hajar GOUMARIR <hajargoumarir7@gmail.com>
2025-12-21 18:32:44 +0100

    Update Main.java

--- src/SIMULATOR6809/CORE/Main.java
+++ src/SIMULATOR6809/CORE/Main.java
@@ -5,10 +5,8 @@ import javax.swing.SwingUtilities;
 class Main {
 
     public static void main(String[] args) {
-        // On lance l'interface graphique sur le thread d'événements de Swing (EDT)
         SwingUtilities.invokeLater(() -> {
             try {
-                // Initialisation du dashboard principal
                 new DASHBOARD();
             } catch (Exception e) {
                 e.printStackTrace();
